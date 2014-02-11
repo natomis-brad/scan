fd95dd0e128691e85f8f8554da8f0efbc6fd6590
Brad Bates <tbbates@gmail.com>
2014-02-10 23:59:26 -0500

    updated

---
+++ scan.js
@@ -0,0 +1,37 @@
+'use strict';
+var fs = require('fs');
+var async = require('async');
+/* indent: 2, node:true, nomen: true, maxlen: 80, vars: true*/
+
+var scan = function(dir, suffix, callback) {
+    fs.readdir(dir, function(err, files) {
+        var returnFiles = [];
+        async.each(files, function(file, next) {
+            var filePath = dir + '/' + file;
+            fs.stat(filePath, function(err, stat) {
+                if (err) {
+                    return next(err);
+                }
+                if (stat.isDirectory()) {
+                    scan(filePath, suffix, function(err, results) {
+                        if (err) {
+                            return next(err);
+                        }
+                        returnFiles = returnFiles.concat(results);
+                        next();
+                    })
+                }
+                else if (stat.isFile()) {
+                    if (file.indexOf(suffix, file.length - suffix.length) !== -1) {
+                        returnFiles.push(filePath);
+                    }
+                    next();
+                }
+            });
+        }, function(err) {
+            callback(err, returnFiles);
+        });
+    });
+};
+
+module.exports = scan;
